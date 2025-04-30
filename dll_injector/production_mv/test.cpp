769a8aae44fb5b1e9f41dabfcb8d62aea91afc5a
QuickscoperXZ <nikitamarkov2u@gmail.com>
2025-04-30 14:55:09 +0300

    Minor changes

--- dll_injector/production_mv/test.cpp
+++ dll_injector/production_mv/test.cpp
@@ -7,42 +7,6 @@
 
 using namespace std;
 
-
-// bool CheckDLLPresence(){
-//     HANDLE hDLLStatusHandle = GetModuleHandleA("testdll.dll");
-//     if (hDLLStatusHandle != NULL){
-//         return true;
-//     }
-//     else return false;    
-// }
-
-// void cls(){
-//     cout << "\033[2J\033[1;1H";
-// }
-
-// void PrintPage(int pageId){
-//     switch (pageId)
-//     {
-//         case 1:
-//             printf("1. CreateProcess\n2. OpenProcess\n3. ExitProcess\n");
-//             break;
-//         case 2:
-//             break;
-//         case 3:
-//             break;
-//         case 4:
-//             break;
-//         case 5:
-//             break;
-//         default:
-//             printf("1. CreateProcess\n2. OpenProcess\n3. ExitProcess\n");
-//             break;
-//     }
-// }
-
-
-
-
 //declaration
 bool bDisableSwitch = true;
 int page = 1;
